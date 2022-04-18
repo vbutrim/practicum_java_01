1faad3379d0fe8a1aa829ea2fdd84c475f69b43a
Ivan Butrim <vbutrim@gmail.com>
2022-04-19 01:19:12 +0300

    add kinopoisk service

---
+++ Sprint8.Kinopoisk/src/main/java/com/vbutrim/kinopoisk/KinopoiskApplication.java
@@ -0,0 +1,13 @@
+package com.vbutrim.kinopoisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinopoiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinopoiskApplication.class, args);
+	}
+
+}
---
+++ Sprint8.Kinopoisk/src/test/java/com/vbutrim/kinopoisk/KinopoiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbutrim.kinopoisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinopoiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

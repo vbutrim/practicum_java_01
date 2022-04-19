a5647bfcd440461fb9cd690a69a35fb0bac2ac2a
Ivan Butrim <vbutrim@gmail.com>
2022-04-19 20:52:01 +0300

    to merge in module-3-sprint-1

---
+++ Sprint8.Kinopoisk/src/test/java/com/vbutrim/kinopoisk/film/api/FilmControllerTest.java
@@ -0,0 +1,35 @@
+package com.vbutrim.kinopoisk.film.api;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author butrim
+ */
+class FilmControllerTest {
+
+    private final FilmController filmController = new FilmController();
+
+    @Test
+    void shouldValidateFilmReleaseDate() {
+        // Given
+        FilmToAddDto filmToAdd = new FilmToAddDto(
+                "Die hard",
+                "too",
+                LocalDate.of(1988, 6, 18),
+                Duration.ofSeconds(10)
+        );
+
+        // When - Then
+        Assertions.assertThrows(
+                FilmController.ValidationException.class,
+                () -> filmController.addNewFilm(filmToAdd)
+        );
+    }
+
+}
